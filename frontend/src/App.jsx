d1ce60a6bd3a39b882b7e93b89a5b4b1f10cee40
Andrii Chychkan <mywork1728@gmail.com>
2026-02-14 19:27:44 +0200

    feat: added cards with fetch query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import ProductCard from "./components/ProductCard.jsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <>
-        <ProductCard/>
-    </>
-  )
+    const [products, setProducts] = useState([])
+    
+const fetch_products = () => {
+    axios.get("http://localhost:8000/products")
+        .then(response => {
+            setProducts(response.data) // <-- ось головне
+        })
+}
+
+    useEffect(() => {
+        fetch_products()
+    }, []);
+
+    return (
+ <div className={"flex flex-wrap gap-4"}>
+        {products.map(product => (
+            <ProductCard
+                key={product.id}
+                name={product.name}
+                description={product.description}
+                image={product.image}
+            />
+        ))}
+    </div>
+    )
 }
 
 export default App
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,16 +1,24 @@
 import {Button, Card} from "antd";
 
 
-function ProductCard() {
-  return (
-    <>
-    <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    </>
-  )
+function ProductCard({name, description, image}) {
+    return (
+        <>
+            <Card title={name} extra={<a href="#">More</a>} style={{width: 300}} hoverable
+                  cover={
+                      <img
+                          draggable={false}
+                          alt={name}
+                          src={image}
+                          onError={(e) => {
+                              e.target.src = "/mock_product.jpg"
+                          }}
+                      />
+                  }>
+                <p>{description}</p>
+            </Card>
+        </>
+    )
 }
 
 export default ProductCard
